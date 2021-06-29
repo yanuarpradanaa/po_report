8b6a37eedfc96ddc3f2a9cfa0aaba090bd1f7ab2
Yanuar Rahmat Yoga Pradana <54838397+yanuarpradanaa@users.noreply.github.com>
2021-06-29 11:18:49 +0700

    Commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name" : "Purchase Order Prima Print", 
+ 	"category" : "Print out Purchase Order", 
+    "version" : "1.0",
+    "depends" : [
+        'purchase',
+    ],
+    "description": """
+Print Out Sale Order""",
+    "author" : "Satusoft",
+    "data" : [
+        'report/po_pp.xml',
+        'report/po_pp_document.xml',
+    ],
+    'installable' : True,
+    'active' : False,
+}
